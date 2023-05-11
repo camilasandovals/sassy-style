09314b1d2d808e43aeba109a5f3c5167a39d2a9a
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-05-11 14:40:24 -0400

    added style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import '../src/styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hello universe</h1>
+      <h2>I am in the milky way</h2>
+      <h3>3rd rock from the sun</h3>
+      <div className='tera'>
+          <h3>Earth</h3>
+            <ul>
+              <li>age: 4.5 billion years</li>
+              <li>radious: 3,9588 miles</li>
+              <li>mass: 5,972 x 10^24kg</li>
+            </ul>
+      </div>
     </div>
   );
 }
